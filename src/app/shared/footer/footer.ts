40b9e1ba8124ec792986b29ac58c96fbf3b40661
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-07-07 19:52:02 -0700

    Added content to footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatDividerModule, MatIconModule, MatButtonModule],
+  imports: [MatDividerModule, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer { }
+export class Footer {
+  protected currentYear: Signal<number> = signal(new Date().getFullYear());
+}
