7cb153cf7886c4faf899f3df7a9ee644c88814d0
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-07-12 19:42:14 -0700

    Created function to show loan summary metric value
    Created function to show loan summary metric value, or show a placeholder value as the user inputs their loan parameters

--- src/app/apps/loan/loan-summary-card/loan-summary-card.ts
+++ src/app/apps/loan/loan-summary-card/loan-summary-card.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { LoanSummaryMetric } from '../loan.models';
+import { isNil } from 'lodash';
 
 @Component({
   selector: 'app-loan-summary-card',
@@ -10,4 +11,23 @@ import { LoanSummaryMetric } from '../loan.models';
 })
 export class LoanSummaryCard {
   @Input() metric!: LoanSummaryMetric;
+
+  /**
+   * Method used to either display loan summary metric value, or show a placeholder value as the user inputs their loan parameters.
+   * @param metric Loan Summary Metric containing metric type, label, value, and display value.
+   * @returns Display value of loan summary metric
+   */
+  showMetricDisplayValue(metric: LoanSummaryMetric): string {
+    let displayValue: string = "";
+    if (metric.metricType === 'rate' && (isNil(metric.value) || !metric.value || metric.displayValue === `${Infinity}%`)) {
+      displayValue = "0.0000%"
+    }
+    else if (metric.metricType === 'amount' && (isNil(metric.value) || !metric.value || displayValue === `$${Infinity}`) || metric.value === Infinity) {
+      displayValue = "$0.00"
+    }
+    else {
+      displayValue = metric.displayValue;
+    }
+    return displayValue;
+  }
 }
