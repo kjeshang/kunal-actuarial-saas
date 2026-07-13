5ca1037eef89da8cf47590a0ea06e5daf5a79c16
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-07-12 23:48:51 -0700

    Created loan amortization schedule models

--- src/app/apps/loan/loan.models.ts
+++ src/app/apps/loan/loan.models.ts
@@ -1,6 +1,26 @@
 export interface LoanSummaryMetric {
-    metricType: 'rate' | 'amount',
-    label: string,
-    value: number,
-    displayValue: string,
+    metricType: 'rate' | 'amount';
+    label: string;
+    value: number;
+    displayValue: string;
+}
+
+export interface LoanAmortizationValue {
+    value: number;
+    displayValue: string;
+}
+
+export interface LoanAmortizationSchedule {
+    // Specific payment interval index
+    period: number;
+    // Time (in years)
+    time: number;
+    // Loan Payment
+    loanPayment: LoanAmortizationValue;
+    // Interest Paid at Time t
+    interestPaid: LoanAmortizationValue;
+    // Principal Repaid at Time t
+    principalRepaid: LoanAmortizationValue;
+    // Outstanding Balance at Time t
+    outstandingBalance: LoanAmortizationValue;
 }
