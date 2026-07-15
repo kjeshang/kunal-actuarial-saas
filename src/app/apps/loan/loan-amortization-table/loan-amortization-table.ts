b72e4f248f2bd02f7dd9852fd99aafee19cfc7a3
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-07-14 23:31:35 -0700

    Created method to handle invalid values when displayed in loan amortization table

--- src/app/apps/loan/loan-amortization-table/loan-amortization-table.ts
+++ src/app/apps/loan/loan-amortization-table/loan-amortization-table.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { LoanStore } from '../loan.store';
+import { isNil } from 'lodash';
 
 @Component({
   selector: 'app-loan-amortization-table',
@@ -11,5 +12,20 @@ import { LoanStore } from '../loan.store';
 export class LoanAmortizationTable {
   loanStore = inject(LoanStore);
   displayedColumns: string[] = ['time', 'loanPayment', 'interestPaid', 'principalRepaid', 'outstandingBalance'];
-  protected readonly Infinity = Infinity;
+
+  /**
+   * Method used to check whether loan payment, interest paid, principal repaid, or outstanding balance values are valid; if not, "$0.00".
+   * @param value Loan Payment, Interest Paid, Principal Repaid, Outstanding Balance
+   * @returns Display value or "$0.00"
+   */
+  showDisplayValue(value: string): string {
+    let displayValue: string = "";
+    if (isNil(value) || !value || value === `$${Infinity}` || value === '-$NaN') {
+      displayValue = "$0.00";
+    }
+    else {
+      displayValue = value;
+    }
+    return displayValue;
+  }
 }
